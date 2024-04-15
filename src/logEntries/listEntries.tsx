a6b923cae0afe7a74045ca95e1bfdec300d83f01
Alif Yasa <130131893+alifyasa@users.noreply.github.com>
2024-04-15 16:30:17 +0700

    feat: Message for empty log

--- src/logEntries/listEntries.tsx
+++ src/logEntries/listEntries.tsx
@@ -7,6 +7,21 @@ interface Props {
     offset?: number
 }
 
+const EmptyEntryMarker = ({ offset = -1 }) => {
+    if (offset === 0) {
+        return (
+            <div class="flex flex-col mb-2 border rounded items-center font-mono flex-grow">
+                <h1 class="m-auto text-xl">Add new logs in <a href="/" class="hover:underline text-blue-600">Home</a>!</h1>
+            </div>
+        )
+    }
+    return (
+        <div class="flex flex-col mb-2 border rounded border-black px-4 py-2 items-center font-mono">
+            <h1>You've reached the top!</h1>
+        </div>
+    )
+}
+
 export const ListEntries = ({ entries, offset = 0 }: Props) => {
     return (
         <>
@@ -17,7 +32,6 @@ export const ListEntries = ({ entries, offset = 0 }: Props) => {
                         <div class="flex flex-col mb-2 border rounded border-black">
                             <div class="py-2 px-4 border-b border-black flex">
                                 <h1 class="text-md font-mono flex-grow">{formatTimestamp(entry.entryTimestamp)}</h1>
-                                {/* <h1 class="text-md font-mono">[#{entry.id}]</h1> */}
                             </div>
                             <div class="flex-grow p-4 text-wrap">
                                 {html(ht as unknown as TemplateStringsArray)}
@@ -28,9 +42,7 @@ export const ListEntries = ({ entries, offset = 0 }: Props) => {
             }
             {
                 entries.length === 0 ? (
-                    <div class="flex flex-col mb-2 border rounded border-black px-4 py-2 items-center font-mono">
-                        <h1>You've reached the top!</h1>
-                    </div>
+                    <EmptyEntryMarker offset={offset} />
                 ) : (
                     <button
                         class="font-mono p-4 border rounded border-black hover:bg-blue-50 mb-2"
